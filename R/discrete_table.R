05872c00d10607274ffbafb520c48c0498a6a1b8
EstherHerbert <e.herbert@sheffield.ac.uk>
2017-09-20 17:00:01 +0100

    Remove sourcing stat_expand

--- R/discrete_table.R
+++ R/discrete_table.R
@@ -1,12 +1,12 @@
 #' Produce a dataframe to summarise discrete variables
-#' 
+#'
 #' @description Takes a dataframe and produces the number and percentage for
 #'              discrete variables.
-#' 
+#'
 #' @param df Data Frame
 #' @param group Variable that defines the grouping
 #' @param variables Variables to be summarised
-#' 
+#'
 #' @export
 discrete_table <- function(df        = .,
                            group     = group,
@@ -14,15 +14,14 @@ discrete_table <- function(df        = .,
                            ...){
   group <- enquo(group)
   variables <- enquo(variables)
-  
+
   # duplicating data to obtail totals
-  source('~/Documents/R/stata_expand.R')
   df <- df %>%
     stata_expand(n = 1) %>%
     mutate(
       !!quo_name(group) := if_else(Duplicate == 1, "Total", as.character(!!group))
     )
-  
+
   new <- df %>%
     select(!!group, !!variables) %>%
     gather(key = variable, value = Scoring, -!!group) %>%
@@ -38,6 +37,6 @@ discrete_table <- function(df        = .,
     ) %>%
     select(-c(n,N,p)) %>%
     spread(key = !!group, value = np)
-  
+
   return(new)
 }
