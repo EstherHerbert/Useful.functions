3d375feead075b7b8e68d886f5c2e33784c3cd0b
EstherHerbert <e.herbert@sheffield.ac.uk>
2017-09-20 12:25:15 +0100

    removed require lines

--- R/read_prospect.R
+++ R/read_prospect.R
@@ -1,25 +1,23 @@
 #' Reads in csv files as exported by Prospect
-#' 
+#'
 #' @description Takes a .csv file exported from Prospect and reads it in to R
 #'              along with the factor labels from the looups.csv file produced
 #'              by Prospect. When using this function you must first read the
 #'              lookups.csv file into R.
-#' 
+#'
 #' @param file The file to be read in
 #' @param lookups The file in which the lookups reside, default is \code{lookups}
 #' @param convert.date Convert fields ending in \code{_dt} to date format
-#' 
+#'
 #' @export
 read_prospect <- function(file,
                           lookups = lookups,
                           convert.date = TRUE){
-  
-  require(lubridate)
-  require(tidyverse)
+
   new <- read.csv(file, stringsAsFactors = FALSE)
   names(new) <- gsub("_o$", "", names(new)) # get rid of _o because this
   # doesn't appear in lookups
-  
+
   # filter the lookup table because there are duplicate fields in different
   # forms
   if(!"form_name" %in% names(new)){ # if there's no form name then check for a parent form name
@@ -35,24 +33,24 @@ read_prospect <- function(file,
     if(!new$form_name[1] %in% lookups$form){
       stop("Form not listed in lookups")
     }
-    L <- filter(lookups, form == new$form_name[1], subform == "", 
+    L <- filter(lookups, form == new$form_name[1], subform == "",
                 field %in% names(new))
   }
   L <- L %>%
     select(-c(form, subform)) %>%
     plyr::dlply(.(field))
-  
+
   if(length(L) > 0){
     # Extract column names, levels, and labels
     cols <- as.character(names(L))
     codes <- lapply(L, "[[", 2)
     labels <- lapply(L, "[[", 3)
-    
+
     for(i in 1:length(L)){
       new[cols[i]] <- factor(new[,cols[i]], levels = codes[[i]], labels = labels[[i]])
     }
   }
-  
+
   # Sort out dates if asked to
   if(convert.date == TRUE){
     dates <- grep("_dt$", names(new))
@@ -62,7 +60,7 @@ read_prospect <- function(file,
       new[,dates] <- lapply(new[,dates], as.Date, format = "%Y-%m-%d")
     }
   }
-  
+
   return(new)
-  
+
 }
