3843c4e4ccfdbb13f308ae6644e4c58d3d3da502
EstherHerbert <e.herbert@sheffield.ac.uk>
2017-09-20 10:07:35 +0100

    Update help page

--- R/stata_expand.R
+++ R/stata_expand.R
@@ -8,6 +8,10 @@
 #' @param df Data frame
 #' @param n Number of times to duplicate (i.e. to end up with 2 copies of everything then n = 1)
 #'
+#' @examples
+#' df <- data.frame(x = 1:5, y = c("a","b","c","d", "e"))
+#' stata_expand(df, 1)
+#'
 #' @export
 stata_expand <- function(df, n){
   old <- cbind(df, data.frame(Duplicate = 0))
