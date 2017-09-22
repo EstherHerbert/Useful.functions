205a72d60b8d33d051f73d9eca638d57a1da1c65
EstherHerbert <e.herbert@sheffield.ac.uk>
2017-09-22 09:48:29 +0100

    Added add_multirow function

---
+++ R/add_multirow.R
@@ -0,0 +1,29 @@
+#' For use with latex summary tables
+#'
+#' @description Turns duplicate rows which occur together into NAs and adds
+#'              multirow to the remaining rows.
+#'
+#' @param x Character vector
+#' @param width Desired width of column to be passed to multirow in latex. e.g.
+#'              \code{"4cm$}
+#'
+#' @examples
+#'     x <- c(rep("a", 5), rep("c", 2), rep("y", 7))
+#'     add_multirow(x)
+#'     add_multirow(x, width = "2cm")
+#'
+#' @export
+add_multirow <- function(x, width = "4cm"){
+
+  oldx <- c(FALSE, x[-1]==x[-length(x)])
+  # is the value equal to the previous?
+  res <- x
+  res[oldx] <- NA
+
+  # add multirow
+  res[!is.na(res)] <- paste0("\\multirow{2}{", width, "}{",
+                             res[!is.na(res)], "}")
+
+  return(res)
+
+}
