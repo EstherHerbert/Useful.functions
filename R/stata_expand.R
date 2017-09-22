71a152a961ed3ad3eecd65e3dcf58c13b4303bb9
EstherHerbert <e.herbert@sheffield.ac.uk>
2017-09-22 14:32:21 +0100

    Updated so that n>1 gives different values for Duplicate

--- R/stata_expand.R
+++ R/stata_expand.R
@@ -2,8 +2,8 @@
 #'
 #' @description Takes a dataframe and duplicates it n times, then creates a
 #'              variable called Duplicate which has a value of 0 if the
-#'              observation originally appeared int he dataset and 1 if the
-#'              observation is one of the duplicates.
+#'              observation originally appeared int he dataset and i = 1,..,n
+#'              for each duplicate.
 #'
 #' @param df Data frame
 #' @param n Number of times to duplicate (i.e. to end up with 2 copies of everything then n = 1)
@@ -15,8 +15,12 @@
 #' @export
 stata_expand <- function(df, n){
   old <- cbind(df, data.frame(Duplicate = 0))
-  copy <- cbind(df, data.frame(Duplicate = 1))
-  new <- rbind(old, copy[rep(seq_len(nrow(copy)), each=n),])
+  copy <- list()
+  for(i in 1:n){
+    copy[[i]] <- cbind(df, data.frame(Duplicate = i))
+  }
+  copy <- do.call("rbind", copy)
+  new <- rbind(old, copy)
   return(new)
 }
 
