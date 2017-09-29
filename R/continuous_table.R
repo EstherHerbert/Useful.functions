5abcc539893172a184b79b6ccbc95b031baa8eb2
EstherHerbert <e.herbert@sheffield.ac.uk>
2017-09-29 10:06:18 +0100

    added extra set argument

--- R/continuous_table.R
+++ R/continuous_table.R
@@ -7,6 +7,8 @@
 #' @param group Variable that defines the grouping
 #' @param variables Variables to be summarised
 #' @param total logical indicating wether a total column should be created
+#' @param set Optional variable that defines extra grouping. This is grouping
+#'            obove the grouping given in \code{group}
 #'
 #' @examples
 #'     continuous_table(df = iris, group = Species,
@@ -17,13 +19,17 @@
 #'
 #' @export
 continuous_table <- function(df        = .,
-                             group     = group,
-                             variables = c(),
-                             total     = TRUE,
-                             ...){
+                              group     = group,
+                              variables = c(),
+                              total     = TRUE,
+                              set       = .,
+                              ...){
   require(tidyverse)
   group <- enquo(group)
   variables <- enquo(variables)
+  if(!missing(set)){
+    set <- enquo(set)
+  }
 
   # duplicating data to obtail totals
   if(total){
@@ -34,35 +40,63 @@ continuous_table <- function(df        = .,
       )
   }
 
-  new <- df %>%
-    select(!!group, !!variables) %>%
-    gather(key = variable, value = value, -!!group) %>%
-    group_by(!!group, variable) %>%
-    summarise(
-      n = sum(!is.na(value)),
-      m = round(mean(value, na.rm = T), 1),
-      sd = round(sd(value, na.rm = T), 2),
-      med = round(median(value, na.rm = T), 1),
-      q25 = round(quantile(value, probs = 0.25, na.rm = T), 1),
-      q75 = round(quantile(value, probs = 0.75, na.rm = T), 1),
-      min = round(min(value, na.rm = T), 1),
-      max = round(max(value, na.rm = T), 1)
-    ) %>%
-    mutate(
-      n = paste0("n = ", n),
-      `Mean (SD)` = paste0(m, " (", sd, ")"),
-      `Median (IQR)` = paste0(med, " (", q25, "-", q75, ")"),
-      `Min - Max` = paste0(min, " - ", max)
-    ) %>%
-    select(-c(m,sd,med,q25,q75,min,max)) %>%
-    gather(key = Scoring, value = value, -!!group, -variable) %>%
-    spread(key = !!group, value = value)
+  if(missing(set)){ # if no higher level setting given
+    new <- df %>%
+      select(!!group, !!variables) %>%
+      gather(key = variable, value = value, -!!group) %>%
+      group_by(!!group, variable) %>%
+      summarise(
+        n = sum(!is.na(value)),
+        m = round(mean(value, na.rm = T), 1),
+        sd = round(sd(value, na.rm = T), 2),
+        med = round(median(value, na.rm = T), 1),
+        q25 = round(quantile(value, probs = 0.25, na.rm = T), 1),
+        q75 = round(quantile(value, probs = 0.75, na.rm = T), 1),
+        min = round(min(value, na.rm = T), 1),
+        max = round(max(value, na.rm = T), 1)
+      ) %>%
+      mutate(
+        n = paste0("n = ", n),
+        `Mean (SD)` = paste0(m, " (", sd, ")"),
+        `Median (IQR)` = paste0(med, " (", q25, "-", q75, ")"),
+        `Min - Max` = paste0(min, " - ", max)
+      ) %>%
+      select(-c(m,sd,med,q25,q75,min,max)) %>%
+      gather(key = Scoring, value = value, -!!group, -variable) %>%
+      spread(key = !!group, value = value)
+  } else { # if higher level setting given
+    new <- df %>%
+      select(!!set, !!group, !!variables) %>%
+      gather(key = variable, value = value, -!!set, -!!group) %>%
+      group_by(!!set, !!group, variable) %>%
+      summarise(
+        n = sum(!is.na(value)),
+        m = round(mean(value, na.rm = T), 1),
+        sd = round(sd(value, na.rm = T), 2),
+        med = round(median(value, na.rm = T), 1),
+        q25 = round(quantile(value, probs = 0.25, na.rm = T), 1),
+        q75 = round(quantile(value, probs = 0.75, na.rm = T), 1),
+        min = round(min(value, na.rm = T), 1),
+        max = round(max(value, na.rm = T), 1)
+      ) %>%
+      mutate(
+        n = paste0("n = ", n),
+        `Mean (SD)` = paste0(m, " (", sd, ")"),
+        `Median (IQR)` = paste0(med, " (", q25, "-", q75, ")"),
+        `Min - Max` = paste0(min, " - ", max)
+      ) %>%
+      select(-c(m,sd,med,q25,q75,min,max)) %>%
+      gather(key = Scoring, value = value, -!!set, -!!group, -variable) %>%
+      unite(temp, !!set, !!group) %>%
+      spread(key = temp, value = value)
+  }
 
+  # make sure
   new$Scoring <- factor(new$Scoring,
                         levels = levels(as.factor(new$Scoring))[c(4,1:3)])
-
   new <- arrange(new, variable, Scoring)
 
   return(new)
 
 }
+
