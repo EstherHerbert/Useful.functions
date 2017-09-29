f298e35d614d3c52865a213dd5c720a1349673b3
EstherHerbert <e.herbert@sheffield.ac.uk>
2017-09-29 12:32:56 +0100

    added set option and tidying at end

--- R/discrete_table.R
+++ R/discrete_table.R
@@ -7,6 +7,8 @@
 #' @param group Variable that defines the grouping
 #' @param variables Variables to be summarised
 #' @param total logical indicating wether a total column should be created
+#' @param set Optional variable that defines extra grouping. This is grouping
+#'            obove the grouping given in \code{group}
 #'
 #' @examples
 #'     library(ggplot2)
@@ -16,10 +18,17 @@
 discrete_table <- function(df        = .,
                            group     = group,
                            variables = c(),
-                           total = TRUE,
+                           total     = TRUE,
+                           set       = .,
                            ...){
+
+  require(tidyverse)
+
   group <- enquo(group)
   variables <- enquo(variables)
+  if(!missing(set)){
+    set <- enquo(set)
+  }
 
   # duplicating data to obtail totals
   if(total){
@@ -30,21 +39,52 @@ discrete_table <- function(df        = .,
       )
   }
 
-  new <- df %>%
-    select(!!group, !!variables) %>%
-    gather(key = variable, value = Scoring, -!!group) %>%
-    group_by(!!group, variable, Scoring) %>%
-    summarise(
-      n = n()
+  if(missing(set)){
+    new <- df %>%
+      select(!!group, !!variables) %>%
+      gather(key = variable, value = Scoring, -!!group) %>%
+      group_by(!!group, variable, Scoring) %>%
+      summarise(
+        n = n()
+      ) %>%
+      group_by(!!group, variable) %>%
+      mutate(
+        N = sum(n),
+        p = round(n/N*100, 1),
+        np = paste0(n, " (", p, "%)")
+      ) %>%
+      select(-c(n,N,p)) %>%
+      spread(key = !!group, value = np)
+  } else {
+    new <- df %>%
+      select(!!set, !!group, !!variables) %>%
+      gather(key = variable, value = Scoring, -!!set, -!!group) %>%
+      group_by(!!set, !!group, variable, Scoring) %>%
+      summarise(
+        n = n()
+      ) %>%
+      group_by(!!set, !!group, variable) %>%
+      mutate(
+        N = sum(n),
+        p = round(n/N*100, 1),
+        np = paste0(n, " (", p, "%)")
+      ) %>%
+      select(-c(n,N,p)) %>%
+      unite(temp, !!set, !!group) %>%
+      spread(key = temp, value = np)
+  }
+
+  new <- new %>%
+    ungroup() %>%
+    select(-(variable:Scoring)) %>%
+    names() %>%
+    mutate_at(new,
+              .,
+              funs(if_else(is.na(.), "0 (0.0%)", .))
     ) %>%
-    group_by(!!group, variable) %>%
     mutate(
-      N = sum(n),
-      p = round(n/N*100, 1),
-      np = paste0(n, " (", p, "%)")
-    ) %>%
-    select(-c(n,N,p)) %>%
-    spread(key = !!group, value = np)
+      Scoring = if_else(is.na(Scoring), "Missing", Scoring)
+    )
 
   return(new)
 
