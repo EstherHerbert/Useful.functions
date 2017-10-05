9d69a164991a0fc9c3aa33e18892b7a58eb1e615
EstherHerbert <e.herbert@sheffield.ac.uk>
2017-10-05 12:34:36 +0100

    Changed function name to round0

--- R/round0.R
+++ R/round0.R
@@ -1,6 +1,6 @@
 #' Rounds numners but keeps trailing zeros
 #'
-#' @description This version of \code{round} is a shortcut for
+#' @description This function is a shortcut for
 #'              \code{formatC(x, digits, format = "f")}.
 #'
 #' @param x a numeric vector
@@ -9,11 +9,11 @@
 #' @return a character string
 #'
 #' @examples
-#' base::round(5.601, 2)
-#' Useful.functions::round(5.601, 2)
+#' round(5.601, 2)
+#' round0(5.601, 2)
 #'
 #' @export
-round <- function(x, digits){
+round0 <- function(x, digits){
   x <- formatC(x, digits = digits, format = "f")
   return(x)
 }
