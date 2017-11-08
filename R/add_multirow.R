14cb696afa54b28382dc181cc31b686aa2370241
EstherHerbert <e.herbert@sheffield.ac.uk>
2017-11-08 13:37:47 +0000

    Added self calculating of rows and ability to reverse

--- R/add_multirow.R
+++ R/add_multirow.R
@@ -9,31 +9,46 @@
 #' @param pos Character defining the vertical positioning of the text in the
 #'            multirow block. Default is "t" - top. Other options are "c" for
 #'            centre or "b" for bottom.
-#' @param rows Number of rows to use
+#' @param rows Optional number of rows to use, if not given then
+#'             \code{add_multirow} calculates how many rows to use.
+#' @param reverse If \code{TRUE} then all by the last duplicate will be
+#'                removed. If rows isn't given then the calculated number of
+#'                rows will be negated. This features is useful when colouring
+#'                tables.
 #'
 #' @return A character string/vector
 #'
 #' @examples
 #'     x <- c(rep("a", 5), rep("c", 2), rep("y", 7))
 #'     add_multirow(x)
+#'     add_multirow(x, reverse = T)
 #'     add_multirow(x, width = "2cm", pos = "c")
 #'
 #' @export
-add_multirow <- function(x, width = "4cm", pos = "t", rows = 2){
+add_multirow <- function(x, width = "4cm", pos = "t", rows = ., reverse = FALSE){
+
+  n <- rle(x[!is.na(x)])$lengths
 
   if(is.factor(x)){
     x <- as.character(x)
   }
 
-  oldx <- c(FALSE, x[-1]==x[-length(x)])
-  # is the value equal to the previous?
-  res <- x
-  res[oldx] <- NA
+  if(!reverse){
+    x[duplicated(x)] <- NA
+
+  } else if(reverse){
+    x[rev(duplicated(rev(x)))] <- NA
+    n <- -1 * n
+  }
+
+  if(missing(rows)){
+    rows <- n
+  }
 
   # add multirow
-  res[!is.na(res)] <- paste0("\\multirow[", pos, "]{",rows,"}{", width, "}{",
-                             res[!is.na(res)], "}")
+  x[!is.na(x)] <- paste0("\\multirow[", pos, "]{",rows,"}{", width, "}{",
+                         x[!is.na(x)], "}")
 
-  return(res)
+  return(x)
 
 }
