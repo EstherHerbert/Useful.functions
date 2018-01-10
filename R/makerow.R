5ef110bafd80b0e1d29d7ecd7fcc282b95a9a306
EstherHerbert <e.herbert@sheffield.ac.uk>
2018-01-10 09:21:45 +0000

    Added new makerow function

---
+++ R/makerow.R
@@ -0,0 +1,25 @@
+#' For use with \code{xtable}'s \code{add.to.row}
+#'
+#' @description Collapses a vector into a character string seperated by "&".
+#'
+#' @param x A vector or single row of a data frame (which is then converted to
+#'          a vector within the function)
+#'
+#' @return A single character string
+#'
+#' @examples
+#'   x <- c("A", 125, "Apple", 0.2, "75g")
+#'   makerow(x)
+#'
+#' @export
+
+makerow <- function(x){
+  if(!is.vector(x)){
+    x <- unlist(x)
+  }
+
+  x[is.na(x)] <- ""
+
+  paste0(x, collapse = " & ")
+
+}
