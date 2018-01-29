951c4a8eb463d183bc795be3d9f0f3698c61ca5e
EstherHerbert <e.herbert@sheffield.ac.uk>
2018-01-29 14:36:12 +0000

    Improved code for continuous table. variables now sorted

--- R/continuous_table.R
+++ R/continuous_table.R
@@ -4,119 +4,107 @@
 #'              such as mean and standard deviation for continuous variables.
 #'
 #' @param df Data frame
+#' @param ... Variables to be summarised
 #' @param group Optional variable that defines the grouping
-#' @param variables Variables to be summarised
 #' @param total Logical indicating wether a total column should be created
 #'
 #' @examples
-#'     continuous_table(df = iris, group = Species,
-#'                      variables = c(Petal.Length, Petal.Width))
-#'     continuous_table(df = iris, group = Species,
-#'                      variables = c(Sepal.Length, Sepal.Width),
+#'     continuous_table(df = iris, Petal.Length, Petal.Width, group = Species)
+#'     continuous_table(df = iris, Sepal.Length, Sepal.Width, group = Species,
 #'                      total = FALSE)
+#'     continuous_table(df = iris, Petal.Length, Sepal.Length)
 #'
 #' @return A tibble data frame summarising the data
 #'
 #' @export
-continuous_table <- function(df        = .,
+continuous_table <- function(df = .,
+                             ...,
                              group = .,
-                             variables = c(),
-                             total     = TRUE,
-                             ...){
-  require(tidyverse)
+                             total = TRUE){
+
+  variables <- quos(...)
   if(!missing(group)){
     group <- enquo(group)
   } else {
     total <- FALSE
   }
-  variables <- enquo(variables)
 
-  # duplicating data to obtail totals
   if(total){
-    df <- df %>%
-      stata_expand(n = 1) %>%
+    df %<>%
+      stata_expand(1) %>%
       mutate(
-        !!quo_name(group) := if_else(Duplicate == 1, "All", as.character(!!group))
+        !!quo_name(group) := if_else(Duplicate == 1,
+                                     "Total",
+                                     as.character(!!group))
       )
   }
 
   if(!missing(group)){
     new <- df %>%
-      select(!!group, !!variables) %>%
-      gather(key = variable, value = value, -!!group)
-
-    dims <- length(unique(new$variable))
-
-    new <- new %>%
+      select(!!group, !!!variables) %>%
+      gather(variable, value, -!!group) %>%
       group_by(!!group, variable) %>%
       summarise(
         N = n(),
+        n = sum(!is.na(value)),
         m = round0(mean(value, na.rm = T), 1),
         sd = round0(sd(value, na.rm = T), 2),
         med = round0(median(value, na.rm = T), 1),
-        q25 = round0(quantile(value, probs = 0.25, na.rm = T), 1),
-        q75 = round0(quantile(value, probs = 0.75, na.rm = T), 1),
-        min = round0(min(value, na.rm = T), 1),
-        max = round0(max(value, na.rm = T), 1),
-        mis = sum(is.na(value)),
-        p = round0(mis/N*100, 1)
+        iqr = round0(IQR(value, na.rm = T), 2),
+        min = min(value, na.rm = T),
+        max = max(value, na.rm = T),
       ) %>%
       mutate(
-        N = paste0("N = ", N),
-        `Mean (SD)` = paste0(m, " (", sd, ")"),
-        `Median (IQR)` = paste0(med, " (", q25, "-", q75, ")"),
-        `Min - Max` = paste0(min, " - ", max),
-        Missing = paste0(mis," (",p,"%)")
+        `Mean(SD)` = paste0(m, " (", sd, ")"),
+        `Median (IQR)` = paste0(med, " (", iqr, ")"),
+        `Min to Max` = paste(min, "to", max)
       ) %>%
-      select(-c(m,sd,med,q25,q75,min,max,mis,p)) %>%
-      gather(key = Scoring, value = value, -!!group, -variable) %>%
-      spread(key = !!group, value = value) %>%
+      ungroup() %>%
+      select(-c(m,sd,med,iqr,min,max))%>%
+      gather(scoring, value, -!!group, -variable) %>%
+      spread(!!group, value) %>%
       mutate(
-        variable = if_else(Scoring == "N", "N", variable)
-      )
-  } else{
+        variable = if_else(scoring == "N", "N", as.character(variable))
+      ) %>%
+      .[!duplicated(.),]
+  } else {
     new <- df %>%
-      select(!!variables) %>%
-      gather(key = variable, value = value)
-
-    dims <- length(unique(new$variable))
-
-    new <- new %>%
+      select(!!!variables) %>%
+      gather(variable, value) %>%
       group_by(variable) %>%
       summarise(
         N = n(),
+        n = sum(!is.na(value)),
         m = round0(mean(value, na.rm = T), 1),
         sd = round0(sd(value, na.rm = T), 2),
         med = round0(median(value, na.rm = T), 1),
-        q25 = round0(quantile(value, probs = 0.25, na.rm = T), 1),
-        q75 = round0(quantile(value, probs = 0.75, na.rm = T), 1),
-        min = round0(min(value, na.rm = T), 1),
-        max = round0(max(value, na.rm = T), 1),
-        mis = sum(is.na(value)),
-        p = round0(mis/N*100, 1)
+        iqr = round0(IQR(value, na.rm = T), 2),
+        min = min(value, na.rm = T),
+        max = max(value, na.rm = T)
       ) %>%
       mutate(
-        N = paste0("N = ", N),
-        `Mean (SD)` = paste0(m, " (", sd, ")"),
-        `Median (IQR)` = paste0(med, " (", q25, "-", q75, ")"),
-        `Min - Max` = paste0(min, " - ", max),
-        Missing = paste0(mis," (",p,"%)")
+        `Mean(SD)` = paste0(m, " (", sd, ")"),
+        `Median (IQR)` = paste0(med, " (", iqr, ")"),
+        `Min to Max` = paste(min, "to", max)
       ) %>%
-      select(-c(m,sd,med,q25,q75,min,max,mis,p)) %>%
-      gather(key = Scoring, value = value, -variable) %>%
+      ungroup() %>%
+      select(-c(m,sd,med,iqr,min,max)) %>%
+      gather(scoring, value, -variable) %>%
       mutate(
-        variable = if_else(Scoring == "N", "N", variable)
-      )
+        variable = if_else(scoring == "N", "N", as.character(variable))
+      ) %>%
+      .[!duplicated(.),]
   }
 
-  new$variable <- as.factor(new$variable) %>%
-    relevel("N")
+  order <- sapply(variables, FUN = quo_name)
 
-  new <- arrange(new, variable)
-
-  if(dims > 1){
-    new <- new[-c(1:dims-1),]
-  }
+  new %<>%
+    mutate(
+      variable = parse_factor(variable, c("N", order)),
+      scoring = as.factor(scoring),
+      scoring = relevel(scoring, "n")
+    ) %>%
+    arrange(variable, scoring)
 
   return(new)
 
