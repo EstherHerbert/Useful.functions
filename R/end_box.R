06e5f00ece090c00053bac1a6f118f11b3f15161
EstherHerbert <e.herbert@sheffield.ac.uk>
2018-11-22 14:41:37 +0000

    new addin to complete header box

---
+++ R/end_box.R
@@ -0,0 +1,10 @@
+end_box <- function(){
+  context <- rstudioapi::getActiveDocumentContext()
+
+  current_col <- context$selection[[1]]$range[[1]][[2]]
+
+  num_spaces <- 80 - current_col - 1
+
+  rstudioapi::insertText(paste0(strrep(" ", num_spaces), "##"))
+
+}
