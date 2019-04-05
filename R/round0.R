a3b402c931a9a1a42451d7a7548037ccc70a866e
EstherHerbert <e.herbert@sheffield.ac.uk>
2019-04-05 13:39:31 +0100

    Revert "handle NAs better"
    This reverts commit 11d9705050a9c2a1eb7e8042484397056e58b0bf.

--- R/round0.R
+++ R/round0.R
@@ -3,7 +3,7 @@
 #' @description This function is a shortcut for
 #'              \code{formatC(x, digits, format = "f")}.
 #'
-#' @param x a number
+#' @param x a numeric vector
 #' @param digits an integer indicating the number of decimal places
 #'
 #' @return a character string
@@ -14,10 +14,6 @@
 #'
 #' @export
 round0 <- function(x, digits) {
-  if (is.na(x)) {
-    x <- NA_character_
-  } else {
-    x <- formatC(x, digits = digits, format = "f")
-  }
+  x <- formatC(x, digits = digits, format = "f")
   return(x)
 }
