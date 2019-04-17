0089cff1d0ce243faf9e2f0f2531d4e4b4ddea16
EstherHerbert <e.herbert@sheffield.ac.uk>
2019-04-17 15:49:00 +0100

    new quick function to apply labels from dataspec

---
+++ R/apply_labels.R
@@ -0,0 +1,27 @@
+#' Apply labels from a dataspec to a column
+#'
+#' @param df Data frame
+#' @param col Variable to be labeled
+#' @param dataspec where to find the labels
+#'
+#' @export
+apply_labels <- function(df, col, dataspec = dataspec){
+
+  col_q <- enquo(col)
+  quo_name(col_q)
+  vars <- filter(table, !is.na(!!col_q))  %>%
+    extract2(quo_name(col_q)) %>%
+    unique() %>%
+    paste0(collapse = "|")
+
+  labs <- dataspec %>%
+    filter(str_detect(Identifier, vars)) %>%
+    mutate(Identifier = str_remove_all(Identifier, "\\[calculated\\] ")) %>%
+    select(Identifier, Label)
+
+  table %>%
+    mutate(
+      !!col_q := factor(!!col_q, levels = labs$Identifier, labels = labs$Label)
+    )
+
+}
