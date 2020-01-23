c177eecd476eac29ce5975683e494a844e2fe1b9
EstherHerbert <e.herbert@sheffield.ac.uk>
2020-01-23 10:59:32 +0000

    added keepLast option

--- R/remove_duplicates.R
+++ R/remove_duplicates.R
@@ -3,6 +3,8 @@
 #' @description Turns duplicate rows into NAs
 #'
 #' @param x Character vector
+#' @param reverse Logical indicating whether the last incidence should be kept
+#'                instead of the first (which is the default).
 #'
 #' @return A character string/vector
 #'
@@ -11,14 +13,14 @@
 #'     remove_duplicates(x)
 #'
 #' @export
-remove_duplicates <- function(x) {
+remove_duplicates <- function(x, keepLast = FALSE) {
   n <- rle(x[!is.na(x)])$lengths
 
   if (is.factor(x)) {
     x <- as.character(x)
   }
 
-  x[duplicated(x)] <- NA
+  x[duplicated(x, fromLast = keepLast)] <- NA
 
   return(x)
 }
