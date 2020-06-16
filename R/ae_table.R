a04f7847ba4ba62f1972faac1afb10a80eae9f62
EstherHerbert <e.herbert@sheffield.ac.uk>
2020-06-16 11:59:22 +0100

    added ability to skip group

--- R/ae_table.R
+++ R/ae_table.R
@@ -10,48 +10,90 @@
 #' @param ID Variable that defines the individual identifier (e.g. screening
 #'           number)
 #' @param accuracy see details of \code{scales::percent}
+#' @param total Logical indicating whether a total column should be created
 #'
 #' @export
-ae_table <- function(df = .,
-                     ...,
-                     group = .,
-                     ID = .,
-                     N = .,
-                     accuracy = 0.1) {
+ae_table <- function (df = .,
+                      ...,
+                      group = .,
+                      ID = .,
+                      N = .,
+                      accuracy = 0.1,
+                      total = FALSE) {
 
   require(tidyverse)
 
   variables <- quos(...)
-  group <- enquo(group)
   ID <- enquo(ID)
 
-  new <- df %>%
-    gather(variable, scoring, !!!variables) %>%
-    group_by(!!group, variable, scoring) %>%
-    summarise(
-      events = n(),
-      across(!!ID, n_distinct, .names = "individuals")
-    ) %>%
-    ungroup() %>%
-    complete(!!group, nesting(variable, scoring),
-             fill = list(events = 0, individuals = 0)) %>%
-    left_join(N) %>%
-    mutate(
-      individuals = paste0(individuals, " (",
-                           scales::percent(individuals/N, accuracy = accuracy),
-                           ")"),
-      N = paste("N =", N)
-    ) %>%
-    gather(stat, value, events, individuals) %>%
-    unite(group, !!group, stat) %>%
-    gather(stat, value, N, value) %>%
-    spread(group, value) %>%
-    mutate_at(
-      vars(variable, scoring),
-      ~if_else(stat == "N", "N", .)
-    ) %>%
-    .[!duplicated(.),] %>%
-    select(-stat)
+  if(!missing(group)) group <- enquo(group) else total <- FALSE
+
+  if(total){
+    df <- df %>%
+      totals(!!group)
+  }
+
+  if(!missing(group)){
+    new <- df %>%
+      pivot_longer(cols = c(!!!variables), names_to = "variable",
+                   values_to = "scoring",
+                   values_transform = list(scoring = as.character)) %>%
+      group_by(!!group, variable, scoring) %>%
+      summarise(
+        events = n(),
+        across(!!ID, n_distinct, .names = "individuals"),
+        .groups = "drop"
+      ) %>%
+      complete(!!group, nesting(variable, scoring),
+               fill = list(events = 0, individuals = 0)
+      ) %>%
+      left_join(N) %>%
+      mutate(
+        individuals = paste0(individuals, " (",
+                             scales::percent(individuals/N,
+                                             accuracy = accuracy), ")"),
+        N = paste("N =", N)
+      ) %>%
+      pivot_longer(events:individuals, names_to = "stat", values_to = "value",
+                   values_transform = list(value = as.character)) %>%
+      unite(group, !!group, stat) %>%
+      pivot_longer(cols = c(N, value), names_to = "stat",
+                   values_to = "value") %>%
+      pivot_wider(names_from = group, values_from = value) %>%
+      mutate(
+        across(c(variable, scoring), ~if_else(stat == "N", "N", .))
+      ) %>%
+      .[!duplicated(.), ] %>%
+      select(-stat)
+  } else {
+    new <- df %>%
+      pivot_longer(cols = c(!!!variables), names_to = "variable",
+                   values_to = "scoring",
+                   values_transform = list(scoring = as.character)) %>%
+      group_by(variable, scoring) %>%
+      summarise(
+        events = n(),
+        across(!!ID, n_distinct, .names = "individuals"),
+        .groups = "drop"
+      ) %>%
+      bind_cols(N) %>%
+      mutate(
+        individuals = paste0(individuals, " (",
+                             scales::percent(individuals/N,
+                                             accuracy = accuracy), ")"),
+        N = paste("N =", N)
+      ) %>%
+      pivot_longer(events:individuals, names_to = "group", values_to = "value",
+                   values_transform = list(value = as.character)) %>%
+      pivot_longer(cols = c(N, value), names_to = "stat",
+                   values_to = "value") %>%
+      pivot_wider(names_from = group, values_from = value) %>%
+      mutate(
+        across(c(variable, scoring), ~if_else(stat == "N", "N", .))
+      ) %>%
+      .[!duplicated(.), ] %>%
+      select(-stat)
+  }
 
   order <- sapply(variables, FUN = quo_name)
 
@@ -59,7 +101,7 @@ ae_table <- function(df = .,
     select(!!!variables) %>%
     mutate_all(~as.factor(.)) %>%
     as.list() %>%
-    map(~ levels(.)) %>%
+    map(~levels(.)) %>%
     unlist(.) %>%
     unname() %>%
     .[!duplicated(.)]
@@ -74,11 +116,12 @@ ae_table <- function(df = .,
           fct_relevel("Missing", after = Inf)
       ) %>%
       arrange(variable, scoring) %>%
-      mutate_at(
-        vars(variable, scoring),
-        ~if_else(scoring == "N", NA_character_, as.character(.))
+      mutate(
+        across(.cols = c(variable, scoring),
+               .fns = ~if_else(scoring == "N", NA_character_, as.character(.)))
       )
   )
 
   return(new)
+
 }
