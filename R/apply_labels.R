cfb72eeea711480f8c7aee2484749281f74de5d0
EstherHerbert <e.herbert@sheffield.ac.uk>
2023-05-26 15:06:23 +0100

    apply_labels to ticked variables

--- R/apply_labels.R
+++ R/apply_labels.R
@@ -3,26 +3,32 @@
 #' @param df Data frame
 #' @param col Variable to be labeled
 #' @param dataspec where to find the labels
+#' @param ticked logical - is the variable a ticked variable?
 #'
 #' @export
-apply_labels <- function(df, col, dataspec = dataspec){
+apply_labels <- function(df, col, dataspec = dataspec, ticked = FALSE){
 
-  col_q <- rlang::enquo(col)
-  rlang::quo_name(col_q)
-  vars <- dplyr::filter(df, !is.na(!!col_q)) %>%
-    magrittr::extract2(rlang::quo_name(col_q)) %>%
+  col <- rlang::enquo(col)
+
+  vars <- dplyr::filter(df, !is.na(!!col))  %>%
+    dplyr::pull(!!col) %>%
     unique() %>%
     paste0(collapse = "|")
 
-  labs <- dataspec %>%
-    dplyr::filter(stringr::str_detect(Identifier, vars)) %>%
-    dplyr::mutate(Identifier = stringr::str_remove_all(Identifier,
-                                                       "\\[calculated\\] ")) %>%
-    dplyr::select(Identifier, Label)
+  if(ticked){
+    labs <- dataspec %>%
+      dplyr::filter(stringr::str_detect(code, vars)) %>%
+      dplyr::select(field = code, field_label = label)
+  } else {
+    labs <- dataspec %>%
+      dplyr::filter(stringr::str_detect(field, vars)) %>%
+      dplyr::mutate(field = stringr::str_remove_all(field, "\\[calculated\\] ")) %>%
+      dplyr::select(field, field_label)
+  }
 
   df %>%
     dplyr::mutate(
-      !!col_q := factor(!!col_q, levels = labs$Identifier, labels = labs$Label)
+      !!col := dplyr::recode(!!col, !!!setNames(labs$field_label, labs$field))
     )
 
 }
