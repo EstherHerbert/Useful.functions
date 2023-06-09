918e3085116e8289d5d2ebb652a6ed6e96f0dc01
EstherHerbert <e.herbert@sheffield.ac.uk>
2023-06-09 09:50:53 +0100

    get width option for addins

--- R/end_box.R
+++ R/end_box.R
@@ -1,9 +1,10 @@
-end_box <- function(){
+end_box <- function(width = getOptions("width", 80)){
+
   context <- rstudioapi::getActiveDocumentContext()
 
   current_col <- context$selection[[1]]$range[[1]][[2]]
 
-  num_spaces <- 80 - current_col - 1
+  num_spaces <- width - current_col - 1
 
   rstudioapi::insertText(location = context$selection[[1]]$range[[1]],
                          text = paste0(strrep(" ", num_spaces), "##"))
--- R/insert_section.R
+++ R/insert_section.R
@@ -1,10 +1,10 @@
-insert_section <- function(){
+insert_section <- function(width = getOption("width", 80)){
 
   context <- rstudioapi::getActiveDocumentContext()
 
   current_col <- context$selection[[1]]$range[[1]][[2]]
 
-  num_dashes <- 80 - current_col + 1
+  num_dashes <- width - current_col + 1
 
   rstudioapi::insertText(location = context$selection[[1]]$range[[1]],
                          text = strrep("-", num_dashes))
